354769c9a6e41515ee1a889850a83ea496b6084f
Jaina <jainamicah@gmail.com>
2025-11-14 15:20:11 +0000

    add favouritebooks component with add/remove handlers

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,22 @@ import { useState } from 'react'
 import './App.css'
 import SearchBox from './components/SearchBox'
 import Books from './components/Books'
+import FavouriteBooks from './components/FavouriteBooks'
 
 
 function App() {
   const [searchTerm, setSearchTerm] = useState('');
+  const [favouriteBooks, setFavouriteBooks] = useState([]);
+
+  const addFavourite = book => {
+    if(!favouriteBooks.some(b => b.title === book.title)){
+      setFavouriteBooks([...favouriteBooks, book])
+    }
+  }
+
+  const removeFavourite = book => {
+    setFavouriteBooks(favouriteBooks.filter(b => b.title !== book.title))
+  }
 
   return (
     <>
